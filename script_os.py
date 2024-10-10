3d802fee7616776f21f1c6ce7518b48e596381ff
Denis <denisgaldin@mail.ru>
2024-10-10 16:39:37 +0300

    Update file and added csv, pdf, xlsx

--- script_os.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILES_DIR = os.path.join(BASE_DIR, 'resources')
-FILES2_DIR = os.path.join(BASE_DIR, 'temp_extracted_files')
-ARCHIVE_FILE = os.path.join(FILES_DIR, 'example.zip')
